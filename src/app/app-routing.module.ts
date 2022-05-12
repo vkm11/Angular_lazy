96b8d63940a75d5be16dbc2e4b5fc62738785ebc
vijaykumar <vijay1111mane@gmail.com>
2022-05-12 11:03:59 +0530

    commit;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'admin', loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
+  },
+  {
+    path: 'user', loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)
   }
 ];
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AdminRoutingModule } from './admin-routing.module';
+import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ListComponent } from './list/list.component';
 
@@ -13,7 +13,7 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    UserRoutingModule
   ]
 })
-export class AdminModule { }
+export class UserModule { }
